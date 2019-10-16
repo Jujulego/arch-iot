eb829497326599dda3aef17e110cfd4066ebff62
Manar21 <manar.aggoun@gmail.com>
2019-10-16 16:45:50 +0200

    Update button.c

--- EX1/button.c
+++ EX1/button.c
@@ -13,19 +13,18 @@ int state = 0;
 
 PROCESS_THREAD(button_process, ev, data) {
   PROCESS_BEGIN();
-  SENSORS_ACTIVATE(button_sensor); //permet d'activer le bouton 
+  SENSORS_ACTIVATE(button_sensor);    //permet d'activer le bouton 
 
   while (1) {
+    //Quand on reçoit un événement et qu'il vient du bouton 
     PROCESS_WAIT_EVENT_UNTIL((ev == sensors_event) && (data == &button_sensor));
-    //Quand on reçoit un événement et que c'est celui viens du bouton 
 
-    if (state) { //si on est dans le state 
-      leds_toggle(LEDS_BLUE);
-      //change l'état de la led Bleue (On/Off) 
+    if (state) {    //si on est dans le state 
+      leds_toggle(LEDS_BLUE); //change l'état de la led Bleue (On/Off) 
     }
 
-    state = !state;
-    //Change l'état du state cela permet de ne pas éteindre si on relache le bouton 
+    state = !state;   //Change l'état du state cela permet de ne pas l'éteindre si on relache le bouton
+     
   }
 
   PROCESS_END();
