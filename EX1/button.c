0d2add5658fe2fce473f8d3dc908d5e9dad31048
Namort <38209685+Namort@users.noreply.github.com>
2019-10-16 16:39:37 +0200

    Commentaires Exo1

--- EX1/button.c
+++ EX1/button.c
@@ -13,16 +13,19 @@ int state = 0;
 
 PROCESS_THREAD(button_process, ev, data) {
   PROCESS_BEGIN();
-  SENSORS_ACTIVATE(button_sensor);
+  SENSORS_ACTIVATE(button_sensor); //permet d'activer le bouton 
 
   while (1) {
     PROCESS_WAIT_EVENT_UNTIL((ev == sensors_event) && (data == &button_sensor));
+    //Quand on reçoit un événement et que c'est celui viens du bouton 
 
-    if (state) {
+    if (state) { //si on est dans le state 
       leds_toggle(LEDS_BLUE);
+      //change l'état de la led Bleue (On/Off) 
     }
 
     state = !state;
+    //Change l'état du state cela permet de ne pas éteindre si on relache le bouton 
   }
 
   PROCESS_END();
