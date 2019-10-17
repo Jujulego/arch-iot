8eb62f2476b9eeb48c4ee2811c1f28e9e5db7339
zitouni <rzitouni@gmail.com>
2019-10-17 05:54:17 -0700

    suite exo2

--- EX2/ex2-unicast.c
+++ EX2/ex2-unicast.c
@@ -12,42 +12,49 @@ AUTOSTART_PROCESSES(&ex2_unicast_process);
 /*---------------------------------------------------------------------------*/
 int wait = 0;
 
-static void
-recv_uc(struct unicast_conn *c, const linkaddr_t *from)
-{
+static void recv_uc(struct unicast_conn *c, const linkaddr_t *from) {
   printf("unicast message received from %d.%d: %s\n",
 	 from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
 
   leds_on(LEDS_GREEN);
-  wait = 5;
+  wait = 2;
 }
 /*---------------------------------------------------------------------------*/
-static void
-sent_uc(struct unicast_conn *c, int status, int num_tx)
-{
+static int success = 0;
+static int failed = 0;
+
+static void sent_uc(struct unicast_conn *c, int status, int num_tx) {
   const linkaddr_t *dest = packetbuf_addr(PACKETBUF_ADDR_RECEIVER);
   if(linkaddr_cmp(dest, &linkaddr_null)) {
     return;
   }
   printf("unicast message sent to %d.%d: status %d num_tx %d\n",
     dest->u8[0], dest->u8[1], status, num_tx);
+
+  if (status == 0) {
+    success++;
+  } else {
+    failed++;
+  }
+
+  printf("success: %d    failed: %d\n", success, failed);
 }
 /*---------------------------------------------------------------------------*/
 static const struct unicast_callbacks unicast_callbacks = {recv_uc, sent_uc};
 static struct unicast_conn uc;
+int i = 0;
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(ex2_unicast_process, ev, data)
-{
+PROCESS_THREAD(ex2_unicast_process, ev, data) {
   PROCESS_EXITHANDLER(unicast_close(&uc);)
 
   PROCESS_BEGIN();
 
-  int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, 20);
+  int rd = NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, 26);
   if (rd == RADIO_RESULT_INVALID_VALUE) {
     printf("Unable to change channel\n");
   }
 
-  rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, 0xFF);
+  rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, 7);
   if (rd == RADIO_RESULT_INVALID_VALUE) {
     printf("Unable to change power\n");
   }
@@ -59,15 +66,19 @@ PROCESS_THREAD(ex2_unicast_process, ev, data)
     linkaddr_t addr;
 
     etimer_set(&et, CLOCK_SECOND);
-
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    packetbuf_copyfrom("Va manger tes grands martin sale tchoin des plages de tes mères", 64);
-    addr.u8[0] = 0x62;//0xb2;
-    addr.u8[1] = 0x44;//0x88;
-    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
-      unicast_send(&uc, &addr);
+    if (i < 10) {
+      packetbuf_copyfrom("Carottes !", 11);
+      addr.u8[0] = 0xb2;
+      addr.u8[1] = 0xec;
+
+      if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
+        unicast_send(&uc, &addr);
+      }
     }
+
+    i++;
 
     if (wait == 0) {
       leds_off(LEDS_GREEN);
