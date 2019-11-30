2655c85bb3db968706a4a536914fe2f967bdc897
Julien Capellari <julien@capellari.net>
2019-11-30 19:07:40 +0100

    cloud implementation

--- projet/cloud.py
+++ projet/cloud.py
@@ -1,44 +1,55 @@
-#!/usr/bin/env python3
 import asyncio
 import aiohttp
 import logging
+import pyee
 import serial_asyncio
 
-from typing import Callable, Optional
+from typing import Dict, Optional, Union
 
 # Constants
 SERIAL_DEVICE = "/dev/ttyUSB0"
 BAUDRATE = 115200
 
-BASE_URL = "http://things.ubidots.com"
 TOKEN = "BBFF-a2Ur7AGWJy97Wrxc9g5QlTs7eVzITD"  # Put your TOKEN here
 DEVICE_LABEL = "ThermoCarrote"  # Put your device label here
 
 
 # Classes
-class SerialProtocol(asyncio.Protocol):
-    def __init__(self, recv_cb: Callable[[str], None]):
+class SerialProtocol(asyncio.Protocol, pyee.AsyncIOEventEmitter):
+    """
+    class SerialProtocol:
+    Manages communication with the device
+
+    Events :
+    - recv (data: str)
+    """
+
+    def __init__(self, *, loop: Optional[asyncio.AbstractEventLoop] = None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+
+        pyee.AsyncIOEventEmitter.__init__(self, loop=loop)
+
         # Attributes
-        self.recv_cb = recv_cb
         self.transport = None  # type: Optional[serial_asyncio.SerialTransport]
-        self.logger = logging.getLogger("serial")
+        self._logger = logging.getLogger("serial")
 
     # Methods
     def connection_made(self, transport: serial_asyncio.SerialTransport):
-        self.logger.info(f'Connected to port {SERIAL_DEVICE}')
+        self._logger.info(f'Connected to port {SERIAL_DEVICE}')
         self.transport = transport
 
     def data_received(self, data: bytes):
         data = data.decode('ascii')
 
-        self.logger.debug(f'Received data: {data[:-1]}')
-        self.recv_cb(data)
+        self._logger.debug(f'Received data: {data[:-1]}')
+        self.emit('recv', data)
 
     def send_data(self, data: str):
         if not data.endswith('\n'):
             data += '\n'
 
-        self.logger.debug(f'Send data: {data[:-1]}')
+        self._logger.debug(f'Send data: {data[:-1]}')
         self.transport.write(data.encode('ascii'))
 
     def connection_lost(self, exc: Optional[Exception]):
@@ -52,94 +63,96 @@ class SerialProtocol(asyncio.Protocol):
         print("resume writing", self.transport.get_write_buffer_size())
 
 
-def send_cloud(packet):
-    url = f'{BASE_URL}/api/v1.6/devices/{DEVICE_LABEL}/'
-    headers = {
-        "X-Auth-Token": TOKEN,
-        "Content-Type": "application/json"
-    }
+class CloudDevice(pyee.AsyncIOEventEmitter):
+    BASE_URL = "http://things.ubidots.com/api/v1.6"
+
+    def __init__(self, name: str, token: str, *, loop: Optional[asyncio.AbstractEventLoop] = None):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+
+        super().__init__(loop=loop)
+
+        # Attributes
+        self.name = name
+        self.token = token
+
+        # - internals
+        self._session = aiohttp.ClientSession()
+        self._logger = logging.getLogger("cloud")
+
+    # Methods
+    async def post_values(self, values: Dict[str, Union[str, int, float]]):
+        url = f'{self.BASE_URL}/devices/{self.name}/'
+        headers = {
+            'X-Auth-Token': self.token,
+            'Content-Type': 'application/json'
+        }
+
+        self._logger.debug(f'Send values: {values}')
+        await self._session.post(url, headers=headers, json=values)
+
+    async def get_value(self, name: str) -> Union[str, int, float]:
+        url = f'{self.BASE_URL}/devices/{self.name}/{name}/lv/'
+        headers = {
+            'X-Auth-Token': self.token,
+            'Content-Type': 'application/json'
+        }
+
+        self._logger.debug(f'Get value of {name}')
+        await with self._session.get(url, headers=headers) as rep:
+            return await rep.json()
+
+    async def watch(self, name: str):
+        value = await self.get_value(name)
+        self.emit(name, value)
+
+        while True:
+            await asyncio.sleep(5, loop=self._loop)
+            nv = await self.get_value(name)
+
+            if nv != value:
+                value = value
+                self.emit(name, value)
+                self._logger.info(f'Value of {name} changed to: {value}')
 
 
-def on_recv(data: str):
-    print(data.strip())
+async def setup(loop: asyncio.AbstractEventLoop):
+    # Connect to device
+    _, protocol = await serial_asyncio.create_serial_connection(
+        loop,
+        lambda : SerialProtocol(loop=loop),
+        SERIAL_DEVICE,
+        baudrate=BAUDRATE
+    )
 
+    # Connect to cloud
+    device = CloudDevice(DEVICE_LABEL, TOKEN, loop=loop)
+    await device.post_values({ "hot": 30 })
 
-def send(protocol: SerialProtocol, cmd: str):
-    protocol.send_data(cmd)
+    loop.create_task(device.watch("hot"))
 
+    # Callbacks
+    @protocol.on("recv")
+    def recv(data: str):
+        if data.startswith('data:'):
+            _, name, value = data.split(':')
+            value = float(value)
 
-def protocol_factory():
-    return SerialProtocol(on_recv)
+            print(f'send : {name} = {value}')
+            device.post_values({ name: value })
 
+        else:
+            print(data)
 
-async def main(loop: asyncio.AbstractEventLoop):
-    _, protocol = await serial_asyncio.create_serial_connection(loop, protocol_factory, SERIAL_DEVICE, baudrate=BAUDRATE)
-
-    loop.call_later(5, lambda: send(protocol, "hot 20"))
-    loop.call_later(10, lambda: send(protocol, "beep"))
+    @device.on("hot")
+    def hot(value: int):
+        print(f'change hot: {value}')
+        protocol.send_data(f'hot {value}')
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
 
-    loop.run_until_complete(main(loop))
+    loop.run_until_complete(setup(loop))
     loop.run_forever()
-
-
-# import time
-# import requests
-# import math
-# import random
-# import json
-#
-#
-#
-#
-# def send_data(payload):
-#     # Creates the headers for the HTTP requests
-#     url =
-#     url = "{}/api/v1.6/devices/{}/".format(url, DEVICE_LABEL)
-#     headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
-#
-#     # Makes the HTTP requests
-#     status = 400
-#     attempts = 0
-#     while status >= 400 and attempts <= 5:
-#         req = requests.get(url=url, headers=headers, data=json.dumps(payload))
-#         status = req.status_code
-#         attempts += 1
-#         time.sleep(1)
-#
-#     # Processes results
-#     if status >= 400:
-#         print("[ERROR] Could not send data after 5 attempts, please check \
-#             your token credentials and internet connection")
-#         return False
-#
-#     #print("[INFO] request made properly, your device is updated")
-#     return True
-#
-#
-# def main():
-#     payload = build_payload(
-#         VARIABLE_LABEL_1, VARIABLE_LABEL_2, VARIABLE_LABEL_3)
-#
-#     print("[INFO] Attemping to send data")
-#     post_request(payload)
-#     print("[INFO] finished")
-#
-#
-# if __name__ == '__main__':
-#     while (True):
-#         data = input()
-#
-#         if data.startswith('data:'):
-#             _, name, value = data.split(':')
-#             value = float(value)
-#
-#             print(f'send : {name} = {value}')
-#             send_data({ name: value })
-#
-#         else:
-#             print(data)
